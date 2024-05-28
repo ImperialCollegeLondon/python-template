278d96f4cf12e7f84688d43409a4048ad20f8fb7
Alex Dewar <a.dewar@imperial.ac.uk>
2024-05-28 16:29:40 +0100

    Revert "Add missing return-type annotation"
    This reverts commit 65d042117d54ac49a4d938cd2f835978d3f8c6dc.

--- {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.project_slug}}.py
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.project_slug}}.py
@@ -3,6 +3,6 @@
 from {{ cookiecutter.project_slug }} import __version__
 
 
-def test_version() -> None:
+def test_version():
     """Check that the version is acceptable."""
     assert __version__ == "0.1.0"
