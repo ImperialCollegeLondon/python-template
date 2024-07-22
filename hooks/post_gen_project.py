a73e186069840f071c1a4106b5e23dc95de7c8e8
Alex Dewar <a.dewar@imperial.ac.uk>
2024-07-22 18:58:16 +0100

    tmp

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,15 +1,16 @@
 import os
 from glob import glob
-
-REMOVE_PATHS = [
-    "{% if not cookiecutter.use_bsd3_licence %}LICENSE{% endif %}",
-    "{% if not cookiecutter.add_precommit_workflows %}.github/workflows/pre-commit*.yml{% endif %}",
-    "{% if not cookiecutter.automerge_bot_prs %}.github/workflows/auto-merge.yml{% endif %}",
-    "{% if cookiecutter.packaging != 'pip-tools' %}*requirements.txt{% endif %}",
-]
+from shutil import which
 
 
-def main():
+def remove_unneeded_files():
+    REMOVE_PATHS = (
+        "{% if not cookiecutter.use_bsd3_licence %}LICENSE{% endif %}",
+        "{% if not cookiecutter.add_precommit_workflows %}.github/workflows/pre-commit*.yml{% endif %}",
+        "{% if not cookiecutter.automerge_bot_prs %}.github/workflows/auto-merge.yml{% endif %}",
+        "{% if cookiecutter.packaging != 'pip-tools' %}*requirements.txt{% endif %}",
+    )
+
     for path in REMOVE_PATHS:
         path = path.strip()
         if path:
@@ -18,6 +19,18 @@ def main():
                     os.unlink(file)
                 else:
                     os.rmdir(file)
+
+
+def init_git_repo():
+    git_path = which("git4")
+    if not git_path:
+        print("WARNING: git executable not found; will not initialise repo")
+        return
+
+
+def main():
+    # remove_unneeded_files()
+    init_git_repo()
 
 
 if __name__ == "__main__":
