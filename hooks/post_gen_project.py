8422e650f6962ef2d90b935d1f86378ee090e842
Alex Dewar <a.dewar@imperial.ac.uk>
2024-07-22 18:53:35 +0100

    post_gen_project.py: Use if __name__ == "__main__" guard
    It's best practice for Python scripts.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,14 +5,20 @@ REMOVE_PATHS = [
     "{% if not cookiecutter.use_bsd3_licence %}LICENSE{% endif %}",
     "{% if not cookiecutter.add_precommit_workflows %}.github/workflows/pre-commit*.yml{% endif %}",
     "{% if not cookiecutter.automerge_bot_prs %}.github/workflows/auto-merge.yml{% endif %}",
-    "{% if cookiecutter.packaging != 'pip-tools' %}*requirements.txt{% endif %}"
+    "{% if cookiecutter.packaging != 'pip-tools' %}*requirements.txt{% endif %}",
 ]
 
-for path in REMOVE_PATHS:
-    path = path.strip()
-    if path:
-        for file in glob(path):
-            if os.path.isfile(file):
-                os.unlink(file)
-            else:
-                os.rmdir(file)
+
+def main():
+    for path in REMOVE_PATHS:
+        path = path.strip()
+        if path:
+            for file in glob(path):
+                if os.path.isfile(file):
+                    os.unlink(file)
+                else:
+                    os.rmdir(file)
+
+
+if __name__ == "__main__":
+    main()
