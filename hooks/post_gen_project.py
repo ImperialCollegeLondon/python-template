08f8e12228a0eb8bf65eca2029776e3177787fa6
Alex Dewar <a.dewar@imperial.ac.uk>
2024-08-06 10:59:23 +0100

    Remove unnecessary use of itertools.chain

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,19 +1,16 @@
 import os
 from glob import glob
-from itertools import chain
 from shutil import rmtree
 
-REMOVE_PATHS = chain(
-    (
-        "{% if not cookiecutter.use_bsd3_licence %}LICENSE{% endif %}",
-        "{% if not cookiecutter.add_precommit_workflows %}.github/workflows/pre-commit*.yml{% endif %}",
-        "{% if not cookiecutter.automerge_bot_prs %}.github/workflows/auto-merge.yml{% endif %}",
-        "{% if cookiecutter.packaging != 'pip-tools' %}requirements.txt{% endif %}",
-        "{% if cookiecutter.packaging != 'pip-tools' %}dev-requirements.txt{% endif %}",
-        "{% if cookiecutter.packaging != 'pip-tools' or not cookiecutter.mkdocs %}doc-requirements.txt{% endif %}",
-        "{% if not cookiecutter.mkdocs %}docs{% endif %}",
-    ),
-    glob("README.*.jinja"),
+REMOVE_PATHS = (
+    "{% if not cookiecutter.use_bsd3_licence %}LICENSE{% endif %}",
+    "{% if not cookiecutter.add_precommit_workflows %}.github/workflows/pre-commit*.yml{% endif %}",
+    "{% if not cookiecutter.automerge_bot_prs %}.github/workflows/auto-merge.yml{% endif %}",
+    "{% if cookiecutter.packaging != 'pip-tools' %}requirements.txt{% endif %}",
+    "{% if cookiecutter.packaging != 'pip-tools' %}dev-requirements.txt{% endif %}",
+    "{% if cookiecutter.packaging != 'pip-tools' or not cookiecutter.mkdocs %}doc-requirements.txt{% endif %}",
+    "{% if not cookiecutter.mkdocs %}docs{% endif %}",
+    "README.*.jinja",
 )
 
 for path in REMOVE_PATHS:
