2de89115fe137031a65d03e447abd9b0bed066c2
Alex Dewar <a.dewar@imperial.ac.uk>
2024-08-06 10:58:11 +0100

    Add option to include MkDocs skeleton

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,13 +1,17 @@
 import os
 from glob import glob
 from itertools import chain
+from shutil import rmtree
 
 REMOVE_PATHS = chain(
     (
         "{% if not cookiecutter.use_bsd3_licence %}LICENSE{% endif %}",
         "{% if not cookiecutter.add_precommit_workflows %}.github/workflows/pre-commit*.yml{% endif %}",
         "{% if not cookiecutter.automerge_bot_prs %}.github/workflows/auto-merge.yml{% endif %}",
-        "{% if cookiecutter.packaging != 'pip-tools' %}*requirements.txt{% endif %}",
+        "{% if cookiecutter.packaging != 'pip-tools' %}requirements.txt{% endif %}",
+        "{% if cookiecutter.packaging != 'pip-tools' %}dev-requirements.txt{% endif %}",
+        "{% if cookiecutter.packaging != 'pip-tools' or not cookiecutter.mkdocs %}doc-requirements.txt{% endif %}",
+        "{% if not cookiecutter.mkdocs %}docs{% endif %}",
     ),
     glob("README.*.jinja"),
 )
@@ -15,8 +19,8 @@ REMOVE_PATHS = chain(
 for path in REMOVE_PATHS:
     path = path.strip()
     if path:
-        for file in glob(path):
-            if os.path.isfile(file):
-                os.unlink(file)
+        for inode in glob(path):
+            if os.path.isfile(inode):
+                os.unlink(inode)
             else:
-                os.rmdir(file)
+                rmtree(path)
